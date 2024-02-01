2263ff99654fdb7f8c03e21e15eec5bb1e22ea25
jose <jose.brito@maisunifacisa.com.br>
2024-01-31 22:01:38 -0300

    fix: novo caminho da api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "http://ec2-52-91-193-174.compute-1.amazonaws.com:8080"
+  apiUrl: "http://ec2-3-234-65-255.compute-1.amazonaws.com:8080"
 };
